0c31b97becb15e67e3a3d18d86dc9b45bca12133
Hage Yaapa <hage.yaapa@in.ibm.com>
2017-03-11 12:01:33 +0530

    loopback

--- 10.js
+++ 10.js
@@ -1,1 +1,3 @@
-console.log('\n  By writing even MORE code, of course! 😎\n')
+var c = require('chalk')
+console.log('\n  By writing even MORE code, of course! 😂😂')
+console.log(c.yellow('          or just use LoopBack\n'))
